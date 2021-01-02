61aca4979ceac55c37d3dd696ade28ac0c291fd2
Shubhendu <60315237+clashin@users.noreply.github.com>
2021-01-02 20:37:42 +0530

    Added campground model
    Added campground model and mongoose database name.

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const app = express();
+
 const path = require('path');
 const methodOverride = require('method-override');
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then((res) => {
-//         console.log('Mongoose Connection open !');
-//     })
-//     .catch((e) => {
-//         console.log('Mongoose Error!');
-//         console.log(e);
-//     })
+const mongoose = require('mongoose');
+
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const app = express();
 
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +26,12 @@ app.set('views', path.join(__dirname, '/views'));
 app.get('/', (req, res) => {
     res.render('home')
 });
+
+app.get('/makecamp', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'No camping allowed' });
+    await camp.save();
+    res.send(camp);
+})
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!');
