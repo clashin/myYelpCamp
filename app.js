da3410a675fbc9970abf703d09d7323dcd771c11
Shubhendu <dragracershekhar@gmail.com>
2021-05-15 23:26:43 +0530

    Added prevention for some security issues
    Added Mongo Injection, Sanitize html.
    Added secure : true in session.
    Added helmet to prevent other issues.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
 
+
 const express = require('express');
 const ejsMate = require('ejs-mate');
 const path = require('path');
@@ -14,6 +15,9 @@ const flash = require('connect-flash');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
+
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -41,14 +45,19 @@ app.set('views', path.join(__dirname, '/views'));
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
 // app.use(express.json());
 
 const sessionConfig = {
+    name: 'season',
     secret: 'Thisshouldbeasecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     } 
@@ -56,6 +65,52 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/clashin/",
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -76,6 +131,7 @@ app.use((req, res, next) => {
         // console.log(req.originalUrl);
         req.session.returnTo = req.originalUrl;
     }
+    // console.log(req.query)
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
--- schemas.js
+++ schemas.js
@@ -1,12 +1,35 @@
-const joi = require('joi');
+const Basejoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const joi = Basejoi.extend(extension);
 
 module.exports.campgroundSchema = joi.object({
     campgrounds: joi.object({
-        title: joi.string().required(),
+        title: joi.string().required().escapeHTML(),
         price: joi.number().required().min(0),
         // image: joi.string().required(),
-        location: joi.string().required(),
-        description: joi.string().required()
+        location: joi.string().required().escapeHTML(),
+        description: joi.string().required().escapeHTML()
     }).required(),
     deleteImages: joi.array()
 })
@@ -14,6 +37,6 @@ module.exports.campgroundSchema = joi.object({
 module.exports.reviewSchema = joi.object({
     review: joi.object({
         rating: joi.number().required().min(1).max(5),
-        body: joi.string().required()
+        body: joi.string().required().escapeHTML()
     }).required()
 })
